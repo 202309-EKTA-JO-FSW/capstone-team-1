11fcdcd93220754d8f38036425895d3d21dbc643
Gorgees Odisho <gorgees041@gmail.com>
2024-02-19 21:51:59 +0300

    fixing user file to point to userController #2

--- server/server/routes/user.js
+++ server/server/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 // profile
-// router.get("/profile/:id", customerController.getCustomerProfile);
-// router.put("/profile/:id", customerController.updateCustomerProfile);
+// router.get("/profile/:id", userController.getUserProfile);
+// router.put("/profile/:id", userController.updateUserProfile);
 
 module.exports = router;
