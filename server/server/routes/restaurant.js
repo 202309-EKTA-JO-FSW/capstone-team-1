5d692d1fb052d5abd66b5e9a635b264d75b46b78
Aya Abu AlHummos <ayaahmad113@gmail.com>
2024-02-24 18:24:08 +0300

    add a line #8

--- server/server/routes/restaurant.js
+++ server/server/routes/restaurant.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const restaurantController = require("../controllers/restaurantController");
 
-// restaurant
+// restaurant 
 // router.get("/", restaurantController.getAllRestaurants);
+
 // router.get("/:resId", restaurantController.getOneRestaurant);
 // router.get("/search", restaurantController.searchRestaurant);
 // router.get("/filter", restaurantController.filterRestaurant);
