985604e514f40b7c086409c2147f9b926edafb49
Hala Qitouqa <h.qitouqa@gmail.com>
2024-02-24 16:14:32 +0300

    added pagination to getAllRestaurantMenuItems function

--- server/server/controllers/restaurantController.js
+++ server/server/controllers/restaurantController.js
@@ -1,16 +1,20 @@
 const restaurantModel = require("../models/restaurantModel");
 const menuItemModel = require("../models/menuItemModel");
 
-
 // get all MenuItems for chosen restaurant
 const getAllRestaurantMenuItems = async (req, res) => {
-   const {resId}= req.params
+  const { resId } = req.params;
+  const page = req.query.page || 0;
+  const itemsPerPage = 10;
   try {
-    const allMenuItems = await restaurantModel.findById(resId).populate({path: "menuItems"});
+    const allMenuItems = await restaurantModel
+      .findById(resId)
+      .populate({ path: "menuItems", options :{
+        skip: page*itemsPerPage, 
+        limit: itemsPerPage
+      }});
     if (!allMenuItems) {
-      res
-        .status(422)
-        .json({ message: "Restaurant doesn't have any Items" });
+      res.status(422).json({ message: "Restaurant doesn't have any Items" });
     } else {
       res.json(allMenuItems);
     }
@@ -21,16 +25,15 @@ const getAllRestaurantMenuItems = async (req, res) => {
 
 const getOneRestaurantMenuItem = async (req, res) => {
   const { resId } = req.params.resId;
-  const {itemId} = req.params.itemId
+  const { itemId } = req.params.itemId;
   try {
-    const restaurantRequested = await restaurantModel.findById({resId})
-    if(restaurantRequested.menuItems){
-      const oneMenuItem = await menuItemModel.findById({itemId})
+    const restaurantRequested = await restaurantModel.findById({ resId });
+    if (restaurantRequested.menuItems) {
+      const oneMenuItem = await menuItemModel.findById({ itemId });
       res.status(200).json(oneMenuItem);
-    }else{
-      res.status(422).json({message: err.message})
+    } else {
+      res.status(422).json({ message: err.message });
     }
-    
   } catch (err) {
     res.status(422).json({ message: err.message });
   }
