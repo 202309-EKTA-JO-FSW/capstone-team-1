69cbbf8765afaf9a982f3e0c0bb88adffcb16649
Hala Qitouqa <h.qitouqa@gmail.com>
2024-02-27 18:26:36 +0300

    added test for menuitems functions

--- server/server/__tests__/restaurantController.test.js
+++ server/server/__tests__/restaurantController.test.js
@@ -36,10 +36,10 @@ async function seedData() {
 
 // Get all menuitems for one restaurant
 describe("GET /:resId/menuItems", () => {
-  test("Should return 201 when menuitems are fetched", async () => {
+  test("Should return 200 when menuitems are fetched", async () => {
     const respond = await request.get("/api/:resId/menuItem");
     expect(respond.status).toBe(200);
-    expect(respond.body.menuItemMock.length).toBe(1);
+    expect(respond.body.menuItemMock.length).not.toBe(0);
   });
 
   test("Should return 422 and message Restaurant doesn't have any MenuItems", async () => {
@@ -53,5 +53,65 @@ describe("GET /:resId/menuItems", () => {
     const respond = await request.get("/api/:resId/menuItem");
 
     expect(respond.status).toBe(500);
+    expect(respond.body.message).toBe(err.message);
+  });
+});
+//get one menuItem from restaurant
+describe("GET /:resId/menuItems/:itemId", () => {
+  test("Should return 200 when one menuItem is fetched", async () => {
+    const respond = await request.get(
+      `/api/:resId/menuItem/${menuItemMock._id}`
+    );
+    expect(respond.status).toBe(200);
+    expect(respond.body.menuItemMock.length).toBe(1);
+  });
+
+  test("Should return 404 and message Menu Item not found", async () => {
+    const respond = await request.get(
+      `/api/:resId/menuItem/${mongoose.Types.ObjectId()}`
+    );
+
+    expect(respond.status).toBe(404);
+    expect(respond.body.message).toBe("Menu Item not found");
+  });
+
+  test("Should return 500", async () => {
+    const respond = await request.get(
+      `/api/:resId/menuItem/${menuItemMock._id}`
+    );
+
+    expect(respond.status).toBe(500);
+    expect(respond.body.message).toBe(err.message);
+  });
+});
+
+// filter menuItems
+describe("GET :resId/menuItems/filter", () => {
+  test("Should return 200 when filter is applied on Menu items", async () => {
+    const respond = await request.get("/:resId/menuItems/filter");
+    expect(respond.status).toBe(200);
+    expect(respond.body.menuItemMock.length).toBe(1);
+  });
+
+  test("Should return 500", async () => {
+    const respond = await request.get("/:resId/menuItems/filter");
+
+    expect(respond.status).toBe(500);
+    expect(respond.body.message).toBe(err.message);
+  });
+});
+//search MenuItems
+describe("GET /:resId/menuItems/search", () => {
+  test("Should return 200 when search for MenuItem is found", async () => {
+    const respond = await request.get("/:resId/menuItems/search");
+    expect(respond.status).toBe(200);
+    expect(respond.body.menuItemMock.length).toBe(1);
+  });
+
+  test("Should return 500", async () => {
+    const respond = await request.get("/:resId/menuItems/search");
+
+    expect(respond.status).toBe(500);
+    expect(respond.body.message).toBe(err.message);
   });
 });
