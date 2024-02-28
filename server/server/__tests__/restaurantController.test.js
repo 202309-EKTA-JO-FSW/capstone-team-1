38afc9cf1963c8865342ff73a97266c74a08e448
Hala Qitouqa <h.qitouqa@gmail.com>
2024-02-28 19:16:07 +0300

    edit first two tests

--- server/server/__tests__/restaurantController.test.js
+++ server/server/__tests__/restaurantController.test.js
@@ -2,7 +2,6 @@ const app = require("../index");
 const request = require("supertest")(app);
 const db = require("../db/connection");
 const mongoose = require("mongoose");
-const { createToken } = require("../controllers/authController");
 const User = require("../models/userModel");
 const Restaurant = require("../models/restaurantModel");
 const MenuItem = require("../models/menuItemModel");
@@ -11,6 +10,7 @@ const {
   customerMock,
   restaurantMock,
   menuItemMock,
+  restaurantMock1,
 } = require("./data");
 
 beforeAll(async () => {
@@ -32,6 +32,7 @@ async function seedData() {
   await User.create(adminMock);
   await User.create(customerMock);
   await Restaurant.create(restaurantMock);
+  await Restaurant.create(restaurantMock1)
 }
 
 // Get all menuitems for one restaurant
@@ -42,24 +43,27 @@ describe("GET /restaurant/menuItems", () => {
       ...menuItemMock,
       restaurant: restaurantMock._id,
     });
+    
   });
   test("Should return 200 when menuitems are fetched", async () => {
     const response = await request
       .get(`/api/restaurant/${restaurantMock._id}/menuItems`)
-      .send();
+    
     expect(response.status).toBe(200);
-    // expect(response.body.restaurantMock.menuItems.length).not.toBe(0);
+    expect(response.body.length).toBe(1);
   });
+
 
   test("Should return 422 and message Restaurant doesn't have any Items", async () => {
     const response = await request.get(
-      `/api/restaurant/${restaurantMock._id}/menuItems`
+      `/api/restaurant/${restaurantMock1._id}/menuItems`
     );
-
     expect(response.status).toBe(422);
     expect(response.body.message).toBe("Restaurant doesn't have any MenuItems");
   });
 });
+
+
 //get one menuItem from restaurant
 describe("GET /:resId/menuItems/:itemId", () => {
   let newMenuItem;
@@ -74,7 +78,7 @@ describe("GET /:resId/menuItems/:itemId", () => {
       `/api/restaurant/${restaurantMock._id}/menuItems/${newMenuItem._id}`
     );
     expect(response.status).toBe(200);
-    //expect(response.body.menuItemMock.length).toBe(1);
+    expect(response.body.length).not.toBe(0);
   });
 
   test("Should return 404 and message Menu Item not found", async () => {
@@ -85,15 +89,6 @@ describe("GET /:resId/menuItems/:itemId", () => {
     expect(response.status).toBe(404);
     expect(response.body.message).toBe("Menu item not found");
   });
-
-  // test("Should return 500", async () => {
-  //   const respond = await request.get(
-  //     `/api/:resId/menuItem/${menuItemMock._id}`
-  //   );
-
-  //   expect(respond.status).toBe(500);
-  //   expect(respond.body.message).toBe(err.message);
-  // });
 });
 
 // filter menuItems
@@ -106,12 +101,7 @@ describe("GET :resId/menuItems/filter", () => {
     //expect(respond.body.menuItemMock.length).toBe(1);
   });
 
-  // test("Should return 500", async () => {
-  //   const respond = await request.get(`/api/restaurant/${restaurantMock._id}/menuItems/filter`);
-
-  //   expect(respond.status).toBe(500);
-  //   expect(respond.body.message).toBe(err.message);
-  // });
+  
 });
 //search MenuItems
 describe("GET /:resId/menuItems/search", () => {
@@ -124,18 +114,9 @@ describe("GET /:resId/menuItems/search", () => {
   });
   test("Should return 200 when search for MenuItem is found", async () => {
     const response = await request.get(
-      `/api/restaurant/${restaurantMock._id}/menuItems/search?type=fastfood`
+      `/api/restaurant/${restaurantMock._id}/menuItems/search?type=${menuItemMock.type}`
     );
     expect(response.status).toBe(200);
     //expect(respond.body.menuItemMock.length).toBe(1);
   });
-
-  // test("Should return 500", async () => {
-  //   const respond = await request.get(
-  //     `/api/restaurant/${restaurantMock._id}/menuItems/search`
-  //   );
-
-  //   expect(respond.status).toBe(500);
-  //   expect(respond.body.message).toBe(err.message);
-  // });
 });
