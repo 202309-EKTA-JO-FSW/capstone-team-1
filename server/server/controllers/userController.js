df0120a7ce6713edf21433c9bddc8af33bbd54db
Nour kayyali <nourkayyali91@gmail.com>
2024-03-01 01:19:43 +0300

    add test for user routes #16

--- server/server/controllers/userController.js
+++ server/server/controllers/userController.js
@@ -4,7 +4,7 @@ const getUserProfile = async (req, res) => {
   try {
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ message: "User not found, should login" });
+      return res.status(401).json({ message: "User not found, should login" });
     }
     return res.status(200).json({ user });
   } catch (error) {
