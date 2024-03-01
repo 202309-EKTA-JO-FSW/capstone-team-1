6a1749229a6f5c07c350b6c283b06b2f2ef5f16c
Nour kayyali <nourkayyali91@gmail.com>
2024-03-02 01:18:04 +0300

    update user controller for udate image #16

--- server/server/controllers/userController.js
+++ server/server/controllers/userController.js
@@ -13,18 +13,50 @@ const getUserProfile = async (req, res) => {
 };
 const updateUserProfile = async (req, res) => {
   const userId = req.userId;
+  const {
+    firstName,
+    lastName,
+    age,
+    gender,
+    phoneNumber,
+    avatar,
+    country,
+    city,
+    street,
+    zipcode,
+  } = req.body;
+
   try {
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
-      { $set: req.body },
-      { new: true }
-    );
-    if (!updatedUser) {
+    const user = await User.findById(userId);
+    if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
+    // get image url
+    // uploadImage arguments uploadImage(imagefile, imageFolder in firebase storage)
+    let imageUrl;
+    if (req.file) {
+      // delete the old image and then update the new one
+      deleteImage(user.avatar);
+      imageUrl = await uploadImage(req.file, "userProfileAvatar");
+      if (!imageUrl) {
+        return res.status(500).json({ message: "Failed to upload image" });
+      }
+    }
+    // update user
+    user.first_name = firstName || user.first_name;
+    user.last_name = lastName || user.last_name;
+    user.avatar = imageUrl || user.avatar;
+    user.age = age || user.age;
+    user.gender = gender || user.gender;
+    user.phoneNumber = phoneNumber || user.phoneNumber;
+    user.country = country || user.country;
+    user.city = city || user.city;
+    user.street = street || user.street;
+    user.zipcode = zipcode || user.zipcode;
+    await user.save();
     res.status(200).json({
       message: "Update user successful",
-      results: updatedUser,
+      results: user,
     });
   } catch (error) {
     return res.status(500).json({ message: error.message });
--- server/server/routes/user.js
+++ server/server/routes/user.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const authUser = require("../middleware/authUser");
+const { uploadMulter } = require("../middleware/multer");
+
+// add multer in middleware to track the file comes from request
+// multer arg should be change according to what the fild of the image
+router.use(uploadMulter.single("image"));
 router.use(authUser);
 // profile
 router.get("/profile", userController.getUserProfile);
