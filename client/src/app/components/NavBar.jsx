25177f783bfd9c66130f70c54505dfda1c760e75
Hala Qitouqa <h.qitouqa@gmail.com>
2024-03-07 12:12:46 +0300

    create basic navbar with links only

--- client/src/app/components/NavBar.jsx
+++ client/src/app/components/NavBar.jsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return <div>
+    <ul>
+      <li>
+    <Link href="/">Home</Link>
+    </li>
+    <li>
+    <Link href="/restaurant">Restaurants</Link>
+    </li>
+    <li>
+    <Link href="/about"> About Us</Link>
+    </li>
+    
+</ul>
+  </div>;
 };
 
 export default NavBar;
