57fced7eb3c34a7927eae05ab4eb8e88c4f27511
Gorgees Odisho <gorgees041@gmail.com>
2024-03-07 07:05:36 +0300

    handle onChange the fields #30

--- client/src/app/components/auth/SignupForm.jsx
+++ client/src/app/components/auth/SignupForm.jsx
@@ -1,8 +1,51 @@
+"use client";
+
+import { useState } from "react";
+
 const SignupForm = () => {
+  const formData = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    age: 0,
+    gender: "",
+    phoneNumber: "",
+    country: "",
+    city: "",
+    street: "",
+    zipcode: "",
+    isAdmin: false,
+  };
+  const [form, setForm] = useState(formData);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+      setForm((prevState) => ({
+        ...prevState,
+        [name]: checked,
+      }));
+    } else {
+      setForm((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center w-full md:w-[600px] p-7">
       <h1 className="text-4xl font-bold my-10">Create an account</h1>
-      <form className="flex flex-col justify-center items-center w-full ">
+      <form
+        className="flex flex-col justify-center items-center w-full"
+        onSubmit={handleSubmit}
+      >
         {/* name */}
         <div className="flex justify-between w-full">
           <input
@@ -10,12 +53,14 @@ const SignupForm = () => {
             name="firstName"
             placeholder="First Name"
             className="w-full p-3 mr-4 field"
+            onChange={handleChange}
           />
           <input
             type="text"
             name="lastName"
             placeholder="Last Name"
             className="w-full p-3 ml-4 field"
+            onChange={handleChange}
           />
         </div>
 
@@ -25,6 +70,7 @@ const SignupForm = () => {
           name="email"
           placeholder="Email"
           className="w-full p-3 field"
+          onChange={handleChange}
         />
 
         {/* password */}
@@ -33,12 +79,14 @@ const SignupForm = () => {
           name="password"
           placeholder="Password"
           className="w-full p-3 field"
+          onChange={handleChange}
         />
         <input
           type="password"
           name="confirmPassword"
           placeholder="Confirm Password"
           className="w-full p-3 field"
+          onChange={handleChange}
         />
 
         <div className="flex justify-between w-full">
@@ -48,6 +96,7 @@ const SignupForm = () => {
             name="age"
             placeholder="Age"
             className="w-full p-3 mr-4 field"
+            onChange={handleChange}
           />
 
           {/* gender */}
@@ -55,6 +104,7 @@ const SignupForm = () => {
             type="text"
             name="gender"
             className="w-full p-3 ml-4 text-gray-700 field"
+            onChange={handleChange}
           >
             <option value="gender">Gender</option>
             <option value="male">Male</option>
@@ -68,6 +118,7 @@ const SignupForm = () => {
           name="phoneNumber"
           placeholder="Phone Number"
           className="w-full p-3 field"
+          onChange={handleChange}
         />
 
         {/* address */}
@@ -77,12 +128,14 @@ const SignupForm = () => {
             name="country"
             placeholder="Country"
             className="w-full p-3 mr-4 field"
+            onChange={handleChange}
           />
           <input
             type="text"
             name="city"
             placeholder="City"
             className="w-full p-3 ml-4 field"
+            onChange={handleChange}
           />
         </div>
         <div className="flex justify-between w-full">
@@ -91,19 +144,27 @@ const SignupForm = () => {
             name="street"
             placeholder="Street"
             className="w-full p-3 mr-4 field"
+            onChange={handleChange}
           />
           <input
             type="number"
             name="zipcode"
             placeholder="Zip code"
             className="w-full p-3 ml-4 field"
+            onChange={handleChange}
           />
         </div>
         <div className="mb-8">
-          <input type="checkbox" name="isAdmin" />
-          <label for="isAdmin"> Sign up as an owner of the restaurant</label>
+          <input type="checkbox" name="isAdmin" onChange={handleChange} />
+          <label htmlFor="isAdmin">
+            {" "}
+            Sign up as an owner of the restaurant
+          </label>
         </div>
-        <button className="bg-main-green py-3 px-8 rounded-3xl text-white text-sm hover:bg-opacity-75">
+        <button
+          type="submit"
+          className="bg-main-green py-3 px-8 rounded-3xl text-white text-sm hover:bg-opacity-75"
+        >
           Sign up
         </button>
       </form>
