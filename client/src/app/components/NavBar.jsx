5dba6406ed0074e1facf36383b56491c18c90101
Hala Qitouqa <h.qitouqa@gmail.com>
2024-03-09 17:39:37 +0300

    added condition from localStorage

--- client/src/app/components/NavBar.jsx
+++ client/src/app/components/NavBar.jsx
@@ -6,7 +6,12 @@ import {GiShoppingCart} from "react-icons/gi"
 import Link from "next/link";
 
 
+
+
 const NavBar = () => {
+  let isLoggedIn = window.localStorage.getItem('token', JSON.stringify(token))
+  let user = window.localStorage.getItem('user', JSON.stringify(user))
+
   return (
     
     <nav className="flex justify-between  w-full sticky bg-white top-0 [font-family:'Poppins-Medium',Helvetica] font-medium ">
@@ -19,10 +24,13 @@ const NavBar = () => {
           <Link href="/customer/cart">
            <GiShoppingCart className="text-2xl hover:text-main-green"/>
           </Link>
-          <p>Hello, Name</p>
-          <Link href="/auth/login">
+          {isLoggedIn
+          ? <p>Hello, {user.name}</p>
+          : <Link href="/auth/login">
           <Button/>
           </Link>
+          
+}
         </section>
        
     </nav>
