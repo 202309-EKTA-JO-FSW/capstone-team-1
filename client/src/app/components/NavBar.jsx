08a1c448d84f60ae6ee36f7cdc4348f8d45f3305
Hala Qitouqa <h.qitouqa@gmail.com>
2024-03-10 21:50:02 +0300

    add avatar next to name if logged in #27

--- client/src/app/components/NavBar.jsx
+++ client/src/app/components/NavBar.jsx
@@ -4,9 +4,9 @@ import Logo from "./navbar/Logo";
 import Btn from "./navbar/Btn";
 import NavLinks from "./navbar/NavLinks";
 import {GiShoppingCart} from "react-icons/gi"
+import { RxAvatar } from "react-icons/rx";
 import Link from "next/link";
-
-
+import Image from "next/image";
 
 
 const NavBar = () => {
@@ -15,7 +15,7 @@ const NavBar = () => {
 
 useEffect(() => {
   const user = JSON.parse(localStorage.getItem('user'));
-  if (!user) {
+  if (user) {
    setUser(user);
   }
 }, []);
@@ -24,23 +24,33 @@ useEffect(() => {
 
   return (
     
-    <nav className="flex justify-between  w-full sticky top-0  [font-family:'Poppins-Medium',Helvetica] font-medium ">
+    <nav className="flex justify-between  w-full sticky top-0 text-black">
         <section className="flex items-center gap-2 flex-wrap  justify-between pl-3 pt-2 py-2">
           <Logo />
           <NavLinks/>
           {/* conditional rendering of My Restaurants if admin is logged in */}
           {(user && user.isAdmin) &&
-           <Link href="/admin/myrestaurant" className= "md:ml-3  text-black hover:text-main-green">My Restaurants</Link>
+           <Link href="/myrestaurant" className= "md:ml-3  text-black hover:text-main-green">My Restaurants</Link>
           }
           
         </section>
         <section className="flex items-center gap-4 flex-wrap  justify-between pr-3 pt-2 py-2">
           <Link href="/customer/cart">
-           <GiShoppingCart className="text-2xl hover:text-main-green"/>
+           <GiShoppingCart className="text-3xl  hover:text-main-green"/>
           </Link>
           {/* condtional rendering of Login button or name of user if logged in */}
-          {user 
-          ? <p>Hello, {user.name}</p>
+          {user.name
+          ? <div className= "flex flex-wrap items-center justify-between " >
+            <Link href={"/profile"}>
+            {/* conditional rendering of avatar */}
+            {user.avatar
+            ? <Image src={user.avatar} alt="User Avatar" width={20} height={25}/>
+            : <RxAvatar className="text-3xl pr-1"/>}
+            </Link>
+            <Link href={"/profile"}>
+            <p className="hover:text-main-green pr-2">Hello, {user.name}</p>
+            </Link>
+            </div>
           : <Link href="/login">
           <Btn text={'LOGIN'}/>
           </Link>
