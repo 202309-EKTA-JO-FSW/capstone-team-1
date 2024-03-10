c3d902d19d2e242c656fe0497398af5860e07305
Hala Qitouqa <h.qitouqa@gmail.com>
2024-03-10 19:31:37 +0300

    add useEffect to access localStorage and fix link routes #27

--- client/src/app/components/NavBar.jsx
+++ client/src/app/components/NavBar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+'use client'
+import React, { useEffect, useState } from "react";
 import Logo from "./navbar/Logo";
 import Button from "./navbar/Button";
 import NavLinks from "./navbar/NavLinks";
@@ -9,9 +10,17 @@ import Link from "next/link";
 
 
 const NavBar = () => {
-  if (typeof window !== 'undefined' && window.localStorage.getItem('user')){
-    user = JSON.parse(window.localStorage.getItem('user'))
+
+  const [user, setUser] = useState({})
+
+useEffect(() => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  if (user) {
+   setUser(user);
   }
+}, []);
+
+
 
   return (
     
@@ -21,7 +30,7 @@ const NavBar = () => {
           <NavLinks/>
           {/* conditional rendering of My Restaurants if admin is logged in */}
           {user.isAdmin &&
-           <Link href="/admin/restaurant" className= "md:ml-3  text-black hover:text-main-green">My Restaurants</Link>
+           <Link href="/admin/myrestaurant" className= "md:ml-3  text-black hover:text-main-green">My Restaurants</Link>
           }
           
         </section>
@@ -32,7 +41,7 @@ const NavBar = () => {
           {/* condtional rendering of Login button or name of user if logged in */}
           {user 
           ? <p>Hello, {user.name}</p>
-          : <Link href="/auth/login">
+          : <Link href="/login">
           <Button/>
           </Link>
           }
