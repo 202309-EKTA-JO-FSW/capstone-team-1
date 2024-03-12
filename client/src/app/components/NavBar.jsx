1978b44dead015be938dfff225ed570c9ef1b31f
Hala Qitouqa <h.qitouqa@gmail.com>
2024-03-12 12:17:20 +0300

    fix avatar padding, add firebase domain #27

--- client/src/app/components/NavBar.jsx
+++ client/src/app/components/NavBar.jsx
@@ -9,9 +9,10 @@ import Link from "next/link";
 import Image from "next/image";
 
 
+
 const NavBar = () => {
 
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({name: "Hala", avatar:"https://illustoon.com/photo/dl/7257.png", isAdmin:true}) 
 
 useEffect(() => {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -28,6 +29,7 @@ useEffect(() => {
         <section className="flex items-center gap-2 flex-wrap  justify-between pl-3 pt-2 py-2">
           <Logo />
           <NavLinks/>
+          
           {/* conditional rendering of My Restaurants if admin is logged in */}
           {(user && user.isAdmin) &&
            <Link href="/myrestaurant" className= "md:ml-3  text-black hover:text-main-green">My Restaurants</Link>
@@ -35,7 +37,7 @@ useEffect(() => {
           
         </section>
         <section className="flex items-center gap-4 flex-wrap  justify-between pr-3 pt-2 py-2">
-          <Link href="/customer/cart">
+          <Link href="/checkout">
            <GiShoppingCart className="text-3xl  hover:text-main-green"/>
           </Link>
           {/* condtional rendering of Login button or name of user if logged in */}
@@ -44,11 +46,13 @@ useEffect(() => {
             <Link href={"/profile"}>
             {/* conditional rendering of avatar */}
             {user.avatar
-            ? <Image src={user.avatar} alt="User Avatar" width={20} height={25}/>
-            : <RxAvatar className="text-3xl pr-1"/>}
+            ? <div
+            className='border border-indigo-200 rounded-full overflow-clip w-[30px] h-[30px]'
+          ><Image src={user.avatar} alt="User Avatar" width={30} height={30}/></div>
+            : <RxAvatar className="text-3xl pr-2"/>}
             </Link>
             <Link href={"/profile"}>
-            <p className="hover:text-main-green pr-2">Hello, {user.name}</p>
+            <p className="hover:text-main-green pr-2 pl-2">Hello, {user.name}</p>
             </Link>
             </div>
           : <Link href="/login">
