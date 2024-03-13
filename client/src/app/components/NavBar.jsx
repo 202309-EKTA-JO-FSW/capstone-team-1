9f690290c30898fcd57cbe4b45e15d823a120a7e
Hala Qitouqa <h.qitouqa@gmail.com>
2024-03-13 21:21:57 +0300

    add placeholder in public folder #27

--- client/src/app/components/NavBar.jsx
+++ client/src/app/components/NavBar.jsx
@@ -6,14 +6,14 @@ import NavLinks from "./navbar/NavLinks";
 import {GiShoppingCart} from "react-icons/gi"
 import Link from "next/link";
 import Image from "next/image";
-
-
+import purplesqr from "../../../public/7257.png"
+import placeholderImage from "../../../public/Avatar-Profile-Image.png"
 
 const NavBar = () => {
 
   const [user, setUser] = useState(()=>{
     const storedUser = JSON.parse(localStorage.getItem('user'))
-    return storedUser || {firstname: "Hala", isAdmin:true}
+    return storedUser || {firstName: "Hala", isAdmin:true}
   });
 
 useEffect(() => {
@@ -24,20 +24,19 @@ useEffect(() => {
 }, []);
 
 
-const placeholderImage = "https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Image.png"
+
   return (
     
     <nav className="flex justify-between  w-full sticky top-0 text-black [font-family:'Inter-Medium',Helvetica] text-lg">
         <section className="flex items-center gap-2 flex-wrap  justify-between pl-3 pt-2 py-2">
           <Logo />
           <NavLinks/>
-          
           {/* conditional rendering of My Restaurants if admin is logged in */}
           {(user && user.isAdmin) &&
            <Link href="/myrestaurant" className= "md:ml-3  text-black hover:text-main-green">My Restaurant</Link>
           }
-          
         </section>
+
         <section className="flex items-center gap-2 flex-wrap pr-3 pt-2 py-2">
           <Link href="/checkout">
            <GiShoppingCart className="text-3xl  hover:text-main-green"/>
@@ -45,17 +44,17 @@ const placeholderImage = "https://www.pngall.com/wp-content/uploads/12/Avatar-Pr
           {/* condtional rendering of Login button or name of user if logged in */}
           {user
           ? <div className= "flex flex-wrap items-center justify-between " >
-            <Link href={"/profile"}>
-            {/* conditional rendering of avatar */}
-            <Image style={{borderRadius: "30px"}} src={user.avatar|| placeholderImage} alt="User Avatar" width={30} height={30}/>
-            </Link>
-            <Link href={"/profile"}>
-            <p className="hover:text-main-green pr-2 pl-2">Hello, {user.firstname}</p>
-            </Link>
+              <Link href={"/profile"}>
+              {/* conditional rendering of avatar */}
+                <Image  src={user.avatar|| placeholderImage} alt="User Avatar" width={30} height={30}  className="rounded-full"/>
+              </Link>
+              <Link href={"/profile"}>
+                <p className="hover:text-main-green pr-2 pl-2">Hello, {user.firstName}</p>
+              </Link>
             </div>
           : <Link href="/login">
-          <Btn text={'LOGIN'}/>
-          </Link>
+              <Btn text={'LOGIN'}/>
+            </Link>
           }
         </section>
        
