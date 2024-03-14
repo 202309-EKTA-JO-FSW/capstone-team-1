f563ab5b75b68fe32594eef892c6efdd429f47e0
Nour kayyali <nourkayyali91@gmail.com>
2024-03-14 03:47:58 +0300

    add css hover on card #28

--- client/src/app/components/Card.jsx
+++ client/src/app/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card({ children }) {
   return (
-    <div className="bg-white shadow-md rounded-lg overflow-hidden">
+    <div className="bg-white shadow-md rounded-lg overflow-hidden  hover:bg-violet-100">
       {children}
     </div>
   );
