eb9474463ea972aeac059ce45e562ce770fd79be
Hala Qitouqa <h.qitouqa@gmail.com>
2024-03-14 20:48:30 +0300

    create contact form component #49

---
+++ client/src/app/components/contact/ContactForm.jsx
@@ -0,0 +1,53 @@
+'use client'
+import React from 'react'
+import Btn from '../Btn';
+const ConactForm = () => {
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+        console.log(data);
+      }
+  return (
+    <div className="flex flex-col items-center justify-center relative top-20 left-2">
+      <h1 className='font-bold text-[24px] p-3'>ConactForm</h1>
+      <form
+        onSubmit={handleSubmit}
+        className='w-full max-w-md'
+      >
+        <div className=' flex flex-col  p-2 '>
+        {/* Name */}
+          <input
+          type="Name"
+          name="Name"
+          placeholder="Name"
+          className="w-full field "
+          autoComplete='off'
+          />
+
+        {/* Email */}
+          <input
+          type="email"
+          name="email"
+          placeholder="E-Mail"
+          className="w-full field  "
+          autoComplete='off'
+          />
+        
+        {/* Message */}
+        
+          <textarea
+          type="message"
+          name="message"
+          placeholder="Message"
+          className="w-full p-6 field h-32 "
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <Btn type="submit" text={"Submit Form"} />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ConactForm
