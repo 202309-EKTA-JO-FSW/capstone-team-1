a9bc0ed5ec5ce5742d058fed34e3432c21839bfa
Gorgees Odisho <gorgees041@gmail.com>
2024-03-15 03:50:06 +0300

    fix user avatar #51

--- client/src/app/components/navbar/User.jsx
+++ client/src/app/components/navbar/User.jsx
@@ -11,12 +11,13 @@ const User = ({ user }) => {
           <div className="flex flex-wrap items-center justify-between ">
             {/* conditional rendering of avatar */}
             <Image
-              src={user.avatar || placeholderImage}
+              src={
+                "https://firebasestorage.googleapis.com/v0/b/capstone-project-486e3.appspot.com/o/restaurantImage%2F3-14-2024_burger.jpg?alt=media&token=aa4fd102-ef3c-458b-a626-8a94b11b8241"
+              }
               alt="User Avatar"
               width={35}
               height={35}
-              className="rounded-full object-cover w-auto h-auto"
-              style={{ clipPath: "circle(50% at 50% 50%)" }}
+              className="rounded-full object-cover w-[35px] h-[35px]"
               priority="true"
             />
             <p className="hover:text-main-green pr-2 pl-2">
