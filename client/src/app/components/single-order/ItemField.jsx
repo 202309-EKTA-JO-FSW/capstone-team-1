dcf9f58eedf75ae8931a5c209208e8a0adb0fee3
Gorgees Odisho <gorgees041@gmail.com>
2024-03-18 08:55:02 +0300

    create cancel & place order btns #60

--- client/src/app/components/single-order/ItemField.jsx
+++ client/src/app/components/single-order/ItemField.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const ItemField = ({ item }) => {
   return (
     <div className="flex justify-between items-center text-base w-full p-1 mb-3 border-b border-[#dedede] capitalize">
-      <p>{item.menuItem.name}</p>
-      <p>{item.quantity}</p>
+      <div className="flex justify-between w-[50%]">
+        <p>{item.menuItem.name}</p>
+        <p>{item.quantity}</p>
+      </div>
       <p>{item.total} JD</p>
     </div>
   );
--- client/src/app/components/single-order/SingleOrder.jsx
+++ client/src/app/components/single-order/SingleOrder.jsx
@@ -5,10 +5,13 @@ import React, { useEffect, useState } from "react";
 import OrderInfo from "./OrderInfo";
 import RestaurantInfo from "./RestaurantInfo";
 import CustomerInfo from "./CustomerInfo";
+import LoadingBtn from "../loading/LoadingBtn";
 
 const SingleOrder = ({ id }) => {
   const [order, setOrder] = useState({});
   const [loading, setLoading] = useState(true);
+  const [cancelLoadingBtn, setCancelLoadingBtn] = useState(false);
+  const [placeOrderLoadingBtn, setPlaceOrderlLoadingBtn] = useState(false);
 
   useEffect(() => {
     // fetch order
@@ -41,6 +44,15 @@ const SingleOrder = ({ id }) => {
       <div className="my-8 w-full flex flex-col md:flex-row  justify-around items-center border border-gray-200 rounded-xl bg-gray-50 p-5">
         <RestaurantInfo restaurant={order.restaurant} />
         <CustomerInfo customer={order.customer} />
+      </div>
+      {/* place order btns */}
+      <div className="w-full flex justify-center items-center">
+        <button className="bg-gray-300 text-white w-full p-3 rounded-lg shadow-md hover:bg-opacity-75 mx-2 h-[48px]">
+          {cancelLoadingBtn ? <LoadingBtn /> : "Cancel"}
+        </button>
+        <button className="bg-main-green text-white w-full p-3 rounded-lg shadow-md hover:bg-opacity-75 mx-2 h-[48px]">
+          {placeOrderLoadingBtn ? <LoadingBtn /> : "Place Order"}
+        </button>
       </div>
     </div>
   );
