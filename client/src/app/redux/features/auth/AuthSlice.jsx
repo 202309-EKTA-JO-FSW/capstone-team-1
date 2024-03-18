a153a8a825947740281280e2d29f53a483816217
Gorgees Odisho <gorgees041@gmail.com>
2024-03-18 19:37:43 +0300

    create login and logout slices #67

--- client/src/app/redux/features/auth/AuthSlice.jsx
+++ client/src/app/redux/features/auth/AuthSlice.jsx
@@ -1,16 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: "User",
+  value: {
+    firstName: "",
+    lastName: "",
+    avatar: "",
+    isAdmin: false,
+    restaurant: "",
+    isLogin: false,
+  },
 };
 
 export const authSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    login: (state, action) => {
+      const { firstName, lastName, avatar, isAdmin, restaurant } =
+        action.payload;
+      return {
+        ...state,
+        firstName,
+        lastName,
+        avatar,
+        isAdmin,
+        restaurant,
+        isLogin: true,
+      };
+    },
+    logout: (state) => initialState,
+  },
 });
 
 // Action creators are generated for each case reducer function
-// export const { increment, decrement, incrementByAmount } = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 
 export default authSlice.reducer;
