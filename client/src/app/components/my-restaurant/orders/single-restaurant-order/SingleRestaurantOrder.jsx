d7775936fe5ecb58cc4ae3eab77cc5a9e9c7e24f
Gorgees Odisho <gorgees041@gmail.com>
2024-03-19 08:49:22 +0300

    handle cancel order from restaurnt side #71

--- client/src/app/components/my-restaurant/orders/single-restaurant-order/SingleRestaurantOrder.jsx
+++ client/src/app/components/my-restaurant/orders/single-restaurant-order/SingleRestaurantOrder.jsx
@@ -1,6 +1,9 @@
 "use client";
 import Loading from "@/app/components/loading/Loading";
-import { fetchSingleRestaurantOrder } from "@/app/lib/data";
+import {
+  fetchSingleRestaurantOrder,
+  fetchUpdateRestaurantOrder,
+} from "@/app/lib/data";
 import React, { useEffect, useState } from "react";
 import RestaurantInfo from "./RestaurantInfo";
 import CustomerInfo from "./CustomerInfo";
@@ -9,11 +12,13 @@ import OrderTime from "./OrderTime";
 import { useRouter } from "next/navigation";
 import RestaurantOrderInfo from "./RestaurantOrderInfo";
 import RestaurantStatus from "./RestaurantStatus";
+import LoadingBtn from "@/app/components/loading/LoadingBtn";
 
 const SingleRestaurantOrder = ({ id }) => {
   const router = useRouter();
   const [order, setOrder] = useState({});
   const [loading, setLoading] = useState(true);
+  const [cancelLoadingBtn, setCancelLoadingBtn] = useState(false);
 
   useEffect(() => {
     // fetch order
@@ -38,6 +43,17 @@ const SingleRestaurantOrder = ({ id }) => {
     );
   }
 
+  const handleCancelOrder = async () => {
+    const orderStatus = await fetchUpdateRestaurantOrder(order._id, {
+      status: "canceled",
+    });
+
+    if (orderStatus) {
+      const updatedOrder = await fetchSingleRestaurantOrder(order._id);
+      setOrder(updatedOrder);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center w-full my-10 px-5 md:px-[120px] lg:px-[250px] xl:px-[400px]">
       <h1 className="text-3xl font-bold text-main-green mb-7">Order</h1>
@@ -51,7 +67,15 @@ const SingleRestaurantOrder = ({ id }) => {
       {/* order time  */}
       <OrderTime order={order} />
 
-      {order.status && <OrderStatus status={order.status} />}
+      {/* cancel btn */}
+      {order.status !== "canceled" && order.status !== "delivered" && (
+        <button
+          onClick={handleCancelOrder}
+          className="bg-red-400 text-white w-full p-3 rounded-lg shadow-md hover:bg-opacity-75 mx-2 h-[48px]"
+        >
+          {cancelLoadingBtn ? <LoadingBtn /> : "Cancel"}
+        </button>
+      )}
     </div>
   );
 };
