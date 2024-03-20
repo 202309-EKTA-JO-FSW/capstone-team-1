54a0b731301407cee7de0da015f75bcdcbed98ae
Hala Qitouqa <h.qitouqa@gmail.com>
2024-03-20 14:28:19 +0300

    create User profile page #75

--- client/src/app/(user)/profile/page.jsx
+++ client/src/app/(user)/profile/page.jsx
@@ -1,7 +1,10 @@
+import UserProfile from "@/app/components/profile/UserProfile";
 import React from "react";
 
 const ProfilePage = () => {
-  return <div className="w-full">ProfilePage</div>;
+  return <div className="w-full">ProfilePage
+  <UserProfile/>
+  </div>;
 };
 
 export default ProfilePage;
