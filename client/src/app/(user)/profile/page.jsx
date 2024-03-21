0bd77f6351e0c3ed9379d69d73932db01108e09b
Hala Qitouqa <h.qitouqa@gmail.com>
2024-03-21 14:54:09 +0300

    finalize user profile with editing #75

--- client/src/app/(user)/profile/page.jsx
+++ client/src/app/(user)/profile/page.jsx
@@ -2,10 +2,11 @@ import UserProfile from "@/app/components/profile/UserProfile";
 import React from "react";
 
 const ProfilePage = () => {
-  
-  return <div className="w-full">ProfilePage
-  <UserProfile />
-  </div>;
+  return (
+    <div className="w-full">
+      <UserProfile />
+    </div>
+  );
 };
 
 export default ProfilePage;
--- client/src/app/components/profile/UserProfile.jsx
+++ client/src/app/components/profile/UserProfile.jsx
@@ -1,257 +1,246 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { fetchUser } from "@/app/lib/data";
+import { fetchUser, fetchUserUpdate } from "@/app/lib/data";
+import Btn from "@/app/components/Btn";
+import Image from "next/image";
+import { FiEdit } from "react-icons/fi";
+import AvatarImg from "../../../../public/Avatar-Profile-Image.png";
+import { La_Belle_Aurore } from "next/font/google";
+import Loading from "../loading/Loading";
 
 const UserProfile = () => {
-  // const [form, setForm] = useState({});
-  // useEffect(() => {
-  //   // Function to fetch user data and update the form state
-  //   const getUserData = async () => {
-  //     try {
-  //       const user = await fetchUser();
-  //       console.log(user);
-  //       // set the form user details
-  //       if (user) {
-  //         setForm({
-  //           phoneNumber: user.phoneNumber || "",
-  //           country: user.address ? user.address.country : "",
-  //           city: user.address ? user.address.city : "",
-  //           street: user.address ? user.address.street : "",
-  //           zipcode: user.address ? user.address.zipcode : "",
-  //         });
-  //       }
-  //     } catch (error) {
-  //       console.error("Error fetching user data:", error);
-  //     }
-  //   };
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    street: "",
+    city: "",
+    country: "",
+    zipcode: 0,
+    avatar: AvatarImg,
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [file, setFile] = useState(AvatarImg);
+  const [disable, setDisable] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  useEffect(() => {
+    const getUserInfo = async () => {
+      try {
+        setIsLoading(true);
+        const user = await fetchUser();
+        console.log(user);
+        if (user) {
+          setForm({
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            avatar: user.avatar || AvatarImg,
+            street: user.address.street,
+            city: user.address.city,
+            zipcode: user.address.zipcode,
+            country: user.address.country,
+          });
+          setIsLoading(false);
+          setDisable(true);
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
 
-  //   // Call the function to fetch user data
-  //   getUserData();
-  // }, []);
-  return <div>UserProfile</div>;
+    // Call the function to fetch user data
+    getUserInfo();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const updatedData = { ...form };
+      updatedData.avatar = file;
+
+      const updateUserInfo = await fetchUserUpdate(updatedData);
+      setForm(updatedData);
+      setDisable(true);
+    } catch {
+      console.error("Error updating user profile:", error);
+    } finally {
+      setIsLoading(false);
+      setIsEditing(false);
+    }
+  };
+
+  const toggleFieldsDisabled = () => {
+    setIsEditing((prev) => !prev);
+    setDisable(false);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center w-full p-1">
+      <h1 className="text-4xl font-bold my-3">User Profile</h1>
+
+      <div className="flex flex-col justify-around w-full ">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <form
+            className="flex flex-col justify-center items-center w-full "
+            onSubmit={handleSubmit}
+          >
+            <div className="flex flex-col justify-around w-full">
+              <div className="flex flex-col p-1">
+                <span className=" h-[200px]  w-[200px] flex justify-center border border-gray-300">
+                  <Image
+                    src={form.avatar || AvatarImg}
+                    width={180}
+                    height={180}
+                    alt="Selected Image Preview"
+                    priority={true}
+                    className="w-auto h-auto rounded"
+                  />
+                </span>
+                {isEditing && (
+                  <input
+                    name={file}
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => setFile(e.target.files[0])}
+                    className="mt-4"
+                  />
+                )}
+
+                <section className="flex justify-end p-2 cursor-pointer hover:text-main-green">
+                  <FiEdit
+                    className="text-2xl "
+                    onClick={toggleFieldsDisabled}
+                  />
+                </section>
+                {/* Name */}
+                <label>First Name:</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
+                  className="w-full field mb-4 pl-2"
+                  autoComplete="name"
+                  value={form.firstName}
+                  required
+                  onChange={handleChange}
+                  disabled={disable}
+                />
+                <label>Last Name:</label>
+
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  className="w-full field mb-4"
+                  autoComplete="name"
+                  value={form.lastName}
+                  required
+                  onChange={handleChange}
+                  disabled={disable}
+                />
+
+                {/* E-Mail */}
+                <label>E-Mail:</label>
+
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="E-Mail"
+                  className="w-full field mb-4"
+                  autoComplete="email"
+                  value={form.email}
+                  onChange={handleChange}
+                  required
+                  disabled={disable}
+                />
+
+                <label>Phone Number:</label>
+
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="Phone Number"
+                  className="w-full field mb-4"
+                  autoComplete="phoneNumber"
+                  value={form.phoneNumber}
+                  onChange={handleChange}
+                  required
+                  disabled={disable}
+                />
+
+                {/* address */}
+                <div className="flex items-center justify-between w-full">
+                  <label className="mb-8">Street: </label>
+
+                  <input
+                    type="text"
+                    name="street"
+                    placeholder="Street"
+                    className="w-full mr-4 field"
+                    value={form.street}
+                    onChange={handleChange}
+                    disabled={disable}
+                  />
+
+                  <label className="mb-8">City:</label>
+
+                  <input
+                    type="text"
+                    name="city"
+                    placeholder="City"
+                    className="w-full mr-4 field"
+                    value={form.city}
+                    onChange={handleChange}
+                    disabled={disable}
+                  />
+                </div>
+                <div className="flex justify-between w-full">
+                  <label>Zip Code:</label>
+
+                  <input
+                    type="number"
+                    name="zipcode"
+                    placeholder="Zip Code"
+                    className="w-full mr-4 field"
+                    value={form.zipcode}
+                    onChange={handleChange}
+                    disabled={disable}
+                  />
+
+                  <label className="mt-2">Country:</label>
+
+                  <input
+                    type="text"
+                    name="country"
+                    placeholder="Country"
+                    className="w-full mr-4 field"
+                    value={form.country}
+                    onChange={handleChange}
+                    disabled={disable}
+                  />
+                </div>
+              </div>
+              {!isLoading && isEditing && (
+                <div className="flex items-center justify-center">
+                  <Btn type="submit" text={"Update Profile"} />
+                </div>
+              )}
+            </div>
+          </form>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
-
-// import React, { useState, useEffect } from "react";
-// import Btn from "@/app/components/Btn";
-// import Image from "next/image";
-// import { fetchUser, fetchUserUpdate } from "@/app/lib/data";
-
-// import { FiEdit } from "react-icons/fi";
-// import { useAppSelector } from "@/app/redux/hooks";
-// import NotFound from "@/app/not-found";
-// const UserProfile =  () => {
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [disable, setDisable] = useState(false);
-//   const [form, setForm] = useState({
-//     firstName: "",
-//     lastName: "",
-//     email: "",
-//     phoneNumber: "",
-//     street: "",
-//     city: "",
-//     zipcode: "",
-//     country: "",
-//   });
-//   const [isSubmitted, setIsSubmitted] = useState(false);
-//   const [file, setFile] = useState();
-//   const [isEditing, setIsEditing] = useState(false);
-
-//   useEffect(() => {
-//     const getUserInfo = async () => {
-//       try {
-//         setIsLoading(true);
-//         const user = await fetchUser();
-//         if (user) {
-//           setForm({
-//             firstName: user.firstName,
-//             lastName: user.lastName,
-//             email: user.email,
-//             phoneNumber: user.phoneNumber,
-//             street: user.address.street,
-//             city: user.address.city,
-//             zipcode: user.address.zipcode,
-//             country: user.address.country,
-//           });
-//         }
-//         setIsLoading(false);
-//       } catch (error) {
-//         console.error("Error fetching user data:", error);
-//       }
-//     };
-//     getUserInfo();
-
-//     setDisable(true);
-//   }, []);
-
-//   // const handleChange = (e) => {
-//   //   const { name, value } = e.target;
-//   //   setForm({ ...form, [name]: value });
-//   //   setIsSubmitted(false);
-//   // };
-
-//   // const handleSubmit = async (e) => {
-//   //   e.preventDefault();
-//   //   const formData = new FormData();
-//   //   formData.append("firstName", form.firstName);
-//   //   formData.append("lastName", form.lastName);
-//   //   formData.append("email", form.email);
-//   //   formData.append("phoneNumber", form.phoneNumber);
-//   //   formData.append("street", form.street);
-//   //   formData.append("city", form.city);
-//   //   formData.append("zipcode", form.zipcode);
-
-//   //   formData.append("country", form.country);
-
-//   //   formData.append("avatar", file);
-//   //   setIsLoading(true);
-//   //   const updateUserInfo = await fetchUserUpdate(formData);
-//   //   setForm(updateUserInfo);
-//   //   setIsLoading(false);
-//   //   setDisable(true);
-//   //   setIsSubmitted(true);
-//   // };
-
-//   // const toggleFieldsDisabled = () => {
-//   //   setIsEditing((prev) => !prev);
-//   //   setDisable(false);
-//   // };
-//   return (
-//     <div>hi</div>
-//     // <div className="flex flex-col justify-center items-center w-full p-2">
-//     //   <h1 className="text-4xl font-bold my-3">User Profile</h1>
-//     //   <div className="flex flex-col justify-around w-full ">
-//     //     {isSubmitted && <p> User Info updated Successfully</p>}
-//     //     <form
-//     //       className="flex flex-col justify-center items-center w-full "
-//     //       onSubmit={handleSubmit}
-//     //     >
-//     //       <div className="flex flex-col justify-around w-full">
-//     //         <div className="flex flex-col p-1">
-//     //           <span className=" h-[160px]  w-[200px] flex justify-center border border-gray-300">
-//     //             {file && (
-//     //               <Image
-//     //                 src={URL.createObjectURL(file)}
-//     //                 width={200}
-//     //                 height={160}
-//     //                 alt="Selected Image Preview"
-//     //                 priority={true}
-//     //                 className="w-auto h-auto rounded"
-//     //               />
-//     //             )}
-//     //           </span>
-//     //           {isEditing && (
-//     //             <input
-//     //               name={file}
-//     //               type="file"
-//     //               accept="image/*"
-//     //               onChange={(e) => setFile(e.target.files[0])}
-//     //               className="mt-4"
-//     //             />
-//     //           )}
-//     //           <section className="flex justify-end p-2 cursor-pointer hover:text-main-green">
-//     //             <FiEdit className="text-2xl " onClick={toggleFieldsDisabled} />
-//     //           </section>
-//     //           {/* Name */}
-//     //           <input
-//     //             type="text"
-//     //             name="firstName"
-//     //             placeholder="First Name"
-//     //             className="w-full field mb-4"
-//     //             autoComplete="name"
-//     //             value={form.firstName}
-//     //             required
-//     //             onChange={handleChange}
-//     //             disabled={disable}
-//     //           />
-
-//     //           <input
-//     //             type="text"
-//     //             name="lastName"
-//     //             placeholder="Last Name"
-//     //             className="w-full field mb-4"
-//     //             autoComplete="name"
-//     //             value={form.lastName}
-//     //             required
-//     //             onChange={handleChange}
-//     //             disabled={disable}
-//     //           />
-//     //           {/* Type */}
-//     //           <input
-//     //             type="email"
-//     //             name="email"
-//     //             placeholder="E-Mail"
-//     //             className="w-full field mb-4"
-//     //             autoComplete="email"
-//     //             value={form.email}
-//     //             onChange={handleChange}
-//     //             required
-//     //             disabled={disable}
-//     //           />
-
-//     //           <input
-//     //             type="number"
-//     //             name="phoneNumber"
-//     //             placeholder="Phone Number"
-//     //             className="w-full field mb-4"
-//     //             autoComplete="phoneNumber"
-//     //             value={form.phoneNumber}
-//     //             onChange={handleChange}
-//     //             required
-//     //             disabled={disable}
-//     //           />
-//     //           {/* address */}
-//     //           <div className="flex items-center justify-between w-full">
-//     //             <input
-//     //               type="text"
-//     //               name="street"
-//     //               placeholder="Street"
-//     //               className="w-full mr-4 field"
-//     //               value={form.street}
-//     //               onChange={handleChange}
-//     //               disabled={disable}
-//     //             />
-//     //             <input
-//     //               type="text"
-//     //               name="city"
-//     //               placeholder="City"
-//     //               className="w-full field"
-//     //               value={form.city}
-//     //               onChange={handleChange}
-//     //               disabled={disable}
-//     //             />
-//     //           </div>
-//     //           <div className="flex justify-between w-full">
-//     //             <input
-//     //               type="number"
-//     //               name="zipcode"
-//     //               placeholder="Zip Code"
-//     //               className="w-full mr-4 field"
-//     //               value={form.zipcode}
-//     //               onChange={handleChange}
-//     //               disabled={disable}
-//     //             />
-//     //             <input
-//     //               type="text"
-//     //               name="country"
-//     //               placeholder="Country"
-//     //               className="w-full field"
-//     //               value={form.country}
-//     //               onChange={handleChange}
-//     //               disabled={disable}
-//     //             />
-//     //           </div>
-//     //         </div>
-//     //         <div className="flex items-center justify-center">
-//     //           <Btn type="submit" text={"Update Profile"} />
-//     //         </div>
-//     //       </div>
-//     //     </form>
-//     //     {isLoading && <p className="font-bold p-2">Pending...</p>}
-//     //   </div>
-//     // </div>
-//   );
-// };
-
-// export default UserProfile;
